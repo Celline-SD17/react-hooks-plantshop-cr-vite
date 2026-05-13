2ed868b1b6d8250e9f03a820cbd80aed7998cc35
CellineJacintaMwaura <jacinta.mwaura@student.moringaschool.com>
2026-05-13 14:48:43 +0300

    First Commit

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+  const [soldOut, setSoldOut] =useState(false);
+    function handleSoldOut(){
+      setSoldOut(!soldOut);
+    }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {soldOut ? (
+        <button className="primary" onClick={handleSoldOut}>Out of Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button className="secondary" onClick={handleSoldOut}>In Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,37 @@
 import React from "react";
+import { useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+    const [plants, setPlants] = useState([]);
+    const [search, setSearch] = useState("");
+  
+  //fetching plants
+    useEffect(() => {
+      fetch("http://localhost:6001/plants")
+       .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch plants!");
+        }
+        return response.json();
+      })
+      .then((data) => setPlants(data))
+      .catch((error) => console.error(error));
+    }, []);
+    //Adding New Plants
+    function handleAddPlant(newPlant){
+      setPlants([...plants, newPlant]);
+    }
+    //filterplants
+    const filteredPlants=plants.filter((plant)=> plant.name.toLowerCase().includes(search.toLowerCase())
+    );
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}  />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants ={filteredPlants} />
     </main>
   );
 }
